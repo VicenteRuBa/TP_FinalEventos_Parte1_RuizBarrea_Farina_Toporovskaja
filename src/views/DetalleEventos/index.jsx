1c40a9b699ebec41fed7dc70e2fda229ee1dfada
eitan <mail@mail.com>
2024-11-29 11:17:12 -0300

    we

--- src/views/DetalleEventos/index.jsx
+++ src/views/DetalleEventos/index.jsx
@@ -30,7 +30,6 @@ const DetalleEvento = () => {
                 setIsSubscribed(isUserSubscribed); // Establecer si el usuario está suscrito
             } catch (error) {
                 console.error('Error fetching event details:', error);
-                setErrorMessage('Error al cargar los detalles del evento.');
             }
         };
 
@@ -40,26 +39,14 @@ const DetalleEvento = () => {
     const handleSubscriptionToggle = async (e) => {
         e.preventDefault();
         if (!ifIsLoggedIn()) {
-            setErrorMessage('Debes iniciar sesión para suscribirte o desuscribirte.');
             return;
         }
 
         const token = localStorage.getItem('token');
-
-        if (isSubscribed) {
-            const confirmUnsubscribe = window.confirm("¿Estás seguro de que quieres desuscribirte?");
-            if (!confirmUnsubscribe) {
-                return;
-            }
-        }
-
         const endpoint = isSubscribed ? 'unsubscription' : 'enrollment'; // Cambiar según el estado de suscripción
-        const method = isSubscribed ? 'delete' : 'post';
 
         try {
-            const response = await axios({
-                method: method,
-                url: `${config.url}api/event/${id}/${endpoint}`,
+            const response = await axios.post(`${config.url}api/event/${id}/${endpoint}`, {}, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
@@ -70,13 +57,7 @@ const DetalleEvento = () => {
                 setErrorMessage(''); // Limpiar mensaje de error si la acción fue exitosa
             }
         } catch (error) {
-            console.error('Error toggling subscription:', error);
-            setErrorMessage(
-                isSubscribed
-                    ? 'Error al desuscribirte. Por favor, intenta nuevamente.'
-                    : 'Error al suscribirte. Por favor, intenta nuevamente.'
-            );
-            if (error.response?.data === "El usuario ya se encuentra registrado en el evento :)") {
+            if (error.response?.data === "El usuario ya se encuentra registrado en el evento") {
                 setIsSubscribed(true); // Si ya está inscrito, cambiar el estado a "suscrito"
                 setErrorMessage("Ya estás inscrito en este evento.");
             } else {
